f847036e6483f66f183dd718f6c347aa43b0d013
Vanessa <vanessa.lima_santos@outlook.com>
2018-08-20 11:47:37 -0300

    Quinto commit - inserindo código do projeto

---
+++ 09. Código/Projeto TDI/Model/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model.Models
+{
+    public class Usuario
+    {
+        [Key] public int Id { get; set; }
+        public bool isAdmin { get; set; }
+        public bool isDisponibilidade { get; set; }
+
+        [Required]
+        [MaxLength(20),MinLength(5)]
+        public string Nome { get; set; }
+        [DisplayName("Nome da Profissão")]
+
+        [Required]
+        [MinLength(4), MaxLength(20)]
+        public string NomeProfissao { get; set; }
+        [DisplayName("Descrição da Profissão")]
+        public string DescricaoProfissao { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required]
+        [Display(Name = "Confirmação de Senha")]
+        [Compare("Senha")]
+        [DataType(DataType.Password)]
+        public string ConfirmarSenha { get; set; }
+    }
+}
---
+++ 09. Código/Projeto TDI/SearchJob/Controllers/UsuarioController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Business;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Model.Models;
+
+namespace SearchJobWeb.Controllers
+{
+    
+    public class UsuarioController : Controller
+    {
+        private GerenciadorUsuario gu;
+        public UsuarioController() => gu = new GerenciadorUsuario();
+
+        // GET: Usuario
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListarUsuarios()
+        {
+            List<Usuario> model = gu.ObterTodos();
+            if (model.Count == 0)
+                model = null;
+            return View(model);
+            
+        }
+
+        // GET: Usuario/Details/5
+        [Route("/Usuario/ListarUsuarios/Details/{id}")]
+        public IActionResult Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                Usuario usuario = gu.ObterById(id);
+                if (usuario != null)
+                    return View(usuario);
+            }
+            return RedirectToAction("ListarUsuarios", "Usuario");
+        }
+
+        // GET: Usuario/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+                gu.Adicionar(usuario);
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Usuario/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                Usuario usuario = gu.ObterById(id);
+                if (usuario != null)
+                    return View(usuario);
+            }
+            return RedirectToAction("ListarUsuarios","Usuario");
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Remove("Id");
+                gu.Editar(usuario);
+                return RedirectToAction("ListarUsuarios", "Usuario");
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuario/Delete/5
+        public IActionResult DeleteAccount(int? id)
+        {
+            if (id.HasValue)
+            {
+                Usuario u = gu.ObterById(id);
+                if (u != null)
+                    return View(u);
+            }
+            return RedirectToAction("ListarUsuarios", "Usuario");
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteAccount(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                gu.Remover(id);
+            }
+            return RedirectToAction("ListarUsuarios", "Usuario");
+        }
+
+
+        //[HttpPost]
+        [ValidateAntiForgeryToken]
+        //[HttpPost,Route("Usuario/PesquisarProfissao/{profissao}")]
+        [HttpPost]
+        public IActionResult PesquisarProfissao(FormPesquisar profissao)
+        {
+            
+            List<Usuario> model = gu.ObterTodosByEmprego(profissao.CampoPesquisar);
+            if (model.Count == 0)
+                model = null;
+            return View(model);
+        }        
+    }
+}
